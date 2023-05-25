0d261c5deb6a38a95931c96a0b17031e7961b2a8
Egg-cellent <stormybringer210@gmail.com>
2023-05-25 13:38:21 +0700

    add prop

---
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CapstoneProject.Areas.Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string ImgPath { get; set; }
+    }
+}
