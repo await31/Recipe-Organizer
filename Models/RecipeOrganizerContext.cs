396503fa5fa4bcfe82b807323fc5c4121062d9f0
ThaiNHSE173633 <thainhse173633@fpt.edu.vn>
2023-05-25 13:48:06 +0700

    a

--- Models/RecipeOrganizerContext.cs
+++ Models/RecipeOrganizerContext.cs
@@ -31,8 +31,18 @@ public partial class RecipeOrganizerContext : IdentityDbContext
     public virtual DbSet<RecipeFeedback> RecipeFeedbacks { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Server= DESKTOP-MQ8F714\\SQLEXPRESS; Database = RecipeOrganizer; Trusted_Connection=True;TrustServerCertificate=True;");
+    {
+        // Create a configuration object
+        var configuration = new ConfigurationBuilder()
+          .SetBasePath(AppDomain.CurrentDomain.BaseDirectory) // Set the base path for the appsettings.json file
+          .AddJsonFile("appsettings.json") // Load the appsettings.json file
+          .Build();
 
+        // Get the connection string
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
