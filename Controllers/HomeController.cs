cfae839083bec972410398ef792c60c9ec9c92b2
khoadha <khoadhase173593@fpt.edu.vn>
2023-06-04 22:24:38 +0700

    update ingredient

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace CapstoneProject.Controllers {
             return View();
         }
 
+        public IActionResult IngredientCategory() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
