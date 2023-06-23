25b995d222ce35667df923c6686f7510aaef2700
ThaiNHSE173633 <thainhse173633@fpt.edu.vn>
2023-06-23 15:56:41 +0700

    fix print for new recipe details

--- Controllers/IngredientsController.cs
+++ Controllers/IngredientsController.cs
@@ -41,14 +41,6 @@ namespace CapstoneProject.Controllers {
             return Json(result);
         }
 
-        [HttpPost]
-        public JsonResult AutoComplete(string term) {
-            var result = (_context.Ingredients.Where(i => i.Status == true).Where(t => t.Name.ToLower().Contains(term.ToLower()))
-                 .Select(t => new { t.Name }))
-                 .ToList();
-            return Json(result);
-        }
-
         [Authorize(Roles = "Admin")]
         [Breadcrumb("Ingredients Management")]
         public IActionResult Index() {
